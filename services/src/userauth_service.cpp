9c4f760cd2fcb0d963b15a34702dc631d4464a33
韩震 <hanzhen10@huawei.com>
2022-03-15 17:38:26 +0800

    fix codex
    Signed-off-by: 韩震 <hanzhen10@huawei.com>
    Change-Id: I51ae621dcc44d9ffe9374246aaaef7d89d36f07d

--- services/src/userauth_service.cpp
+++ services/src/userauth_service.cpp
@@ -138,7 +138,7 @@ void UserAuthService::GetProperty(const GetPropertyRequest request, sptr<IUserAu
         USERAUTH_HILOGE(MODULE_SERVICE, "Failed to add death recipient UserAuthServiceCallbackDeathRecipient");
     }
 
-    callerID = this->GetCallingUid();
+    callerID = (uint64_t)this->GetCallingUid();
     callerName = std::to_string(callerID);
 
     userauthController_.GetPropAuthInfo(userID, callerName, callerID, request, callback);
@@ -165,7 +165,7 @@ void UserAuthService::SetProperty(const SetPropertyRequest request, sptr<IUserAu
         USERAUTH_HILOGE(MODULE_SERVICE, "Failed to add death recipient UserAuthServiceCallbackDeathRecipient");
     }
 
-    callerID = this->GetCallingUid();
+    callerID = (uint64_t)this->GetCallingUid();
     callerName = std::to_string(callerID);
 
     ret = userauthController_.SetExecutorProp(callerID, callerName, request, callback);
@@ -322,7 +322,7 @@ int32_t UserAuthService::GetControllerData(sptr<IUserAuthCallback> &callback, Au
         return FAIL;
     }
 
-    callerID = this->GetCallingUid();
+    callerID = (uint64_t)this->GetCallingUid();
     callerName = std::to_string(callerID);
 
     if (userauthController_.GenerateContextID(contextID) != SUCCESS) {
