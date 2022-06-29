d9b6a4c954f03149efea1b76859c21ef61c46a8d
Tianshi Liu <tianshi.liu@huawei.com>
2022-06-29 10:21:51 +0800

    add auto executor mgr status listener
    Change-Id: If974106949fc848bb0b5b2077a12fca83feb8c5a
    Signed-off-by: Tianshi Liu <tianshi.liu@huawei.com>

---
+++ common/executors/src/listener/auth_executor_mgr_status_listener.cpp
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "auth_executor_mgr_status_listener.h"
+
+#include "driver_manager.h"
+#include "iam_logger.h"
+#include "system_ability_definition.h"
+
+#define LOG_LABEL Common::LABEL_USER_AUTH_EXECUTOR
+
+namespace OHOS {
+namespace UserIAM {
+namespace UserAuth {
+sptr<AuthExecutorMgrStatusListener> AuthExecutorMgrStatusListener::GetInstance()
+{
+    static sptr<AuthExecutorMgrStatusListener> instance = new (std::nothrow) AuthExecutorMgrStatusListener();
+    if (instance == nullptr) {
+        IAM_LOGE("instance is nullptr");
+    }
+    return instance;
+}
+
+void AuthExecutorMgrStatusListener::OnAddSystemAbility(int32_t systemAbilityId, const std::string &deviceId)
+{
+    if (systemAbilityId != SUBSYS_USERIAM_SYS_ABILITY_AUTHEXECUTORMGR) {
+        return;
+    }
+
+    IAM_LOGD("auth executor mgr SA added");
+}
+
+void AuthExecutorMgrStatusListener::OnRemoveSystemAbility(int32_t systemAbilityId, const std::string &deviceId)
+{
+    if (systemAbilityId != SUBSYS_USERIAM_SYS_ABILITY_AUTHEXECUTORMGR) {
+        return;
+    }
+
+    IAM_LOGE("auth executor mgr SA removed");
+}
+} // namespace UserAuth
+} // namespace UserIAM
+} // namespace OHOS
