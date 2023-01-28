b934d270f849965fb9517ac0ced312e6b8f2f520
youliang_1314 <youliang4@huawei.com>
2023-01-28 11:39:57 +0000

    remove redundant symbol
    Signed-off-by: youliang_1314 <youliang4@huawei.com>
    Change-Id: I3a7dfaca3484b9df2193152bf48cbbcc3dd5631b

--- frameworks/js/napi/user_auth/src/auth_instance_v9.cpp
+++ frameworks/js/napi/user_auth/src/auth_instance_v9.cpp
@@ -328,7 +328,7 @@ ResultCodeV9 AuthInstanceV9::Cancel(napi_env env, napi_callback_info info)
         IAM_LOGE("auth not started");
         return ResultCodeV9::GENERAL_ERROR;
     }
-    int32_t result = UserAuthClientImpl::Instance().CancelAuthentication(contextId_);
+    int32_t result = UserAuthClient::GetInstance().CancelAuthentication(contextId_);
     if (result != ResultCode::SUCCESS) {
         IAM_LOGE("CancelAuthentication fail:%{public}d", result);
         return ResultCodeV9(UserAuthNapiHelper::GetResultCodeV9(result));
