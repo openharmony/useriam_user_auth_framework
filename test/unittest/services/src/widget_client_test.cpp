c3c772eee8e3dadd3612125548ec5e331b21e72b
liuziweicom <liuziwei12@huawei.com>
2023-07-31 15:07:37 +0800

    add for widget UT
    Signed-off-by: liuziweicom <liuziwei12@huawei.com>

--- test/unittest/services/src/widget_client_test.cpp
+++ test/unittest/services/src/widget_client_test.cpp
@@ -67,24 +67,28 @@ HWTEST_F(WidgetClientTest, WidgetClientTestSetWidgetSchedule, TestSize.Level0)
 {
     auto schedule = std::make_shared<WidgetScheduleNodeImpl>();
     WidgetClient::Instance().SetWidgetSchedule(schedule);
+    EXPECT_NE(schedule, nullptr);
 }
 
 HWTEST_F(WidgetClientTest, WidgetClientTestSetWidgetContextId, TestSize.Level0)
 {
     uint64_t contextId = 6;
     WidgetClient::Instance().SetWidgetContextId(contextId);
+    EXPECT_EQ(contextId, 6);
 }
 
 HWTEST_F(WidgetClientTest, WidgetClientTestSetWidgetParam, TestSize.Level0)
 {
     WidgetParam widgetParam;
     WidgetClient::Instance().SetWidgetParam(widgetParam);
+    EXPECT_EQ(widgetParam.title, "");
 }
 
 HWTEST_F(WidgetClientTest, WidgetClientTestSetWidgetCallback, TestSize.Level0)
 {
     sptr<WidgetCallbackInterface> testCallback(nullptr);
     WidgetClient::Instance().SetWidgetCallback(testCallback);
+    EXPECT_EQ(WidgetClient::Instance().GetAuthTokenId(), 0);
 }
 
 HWTEST_F(WidgetClientTest, WidgetClientTestSetAuthTokenId, TestSize.Level0)
@@ -102,6 +106,12 @@ HWTEST_F(WidgetClientTest, WidgetClientTestOnNotice_001, TestSize.Level0)
 
 HWTEST_F(WidgetClientTest, WidgetClientTestOnNotice_002, TestSize.Level0)
 {
+    std::string eventData = "";
+    EXPECT_EQ(WidgetClient::Instance().OnNotice((NoticeType)0, eventData), ResultCode::INVALID_PARAMETERS);
+}
+
+HWTEST_F(WidgetClientTest, WidgetClientTestOnNotice_003, TestSize.Level0)
+{
     WidgetNotice widgetNotice;
     widgetNotice.widgetContextId = 1;
     widgetNotice.event = "EVENT_AUTH_TYPE_READY";
@@ -112,10 +122,10 @@ HWTEST_F(WidgetClientTest, WidgetClientTestOnNotice_002, TestSize.Level0)
     widgetNotice.version = "1";
     nlohmann::json root = widgetNotice;
     std::string eventData = root.dump();
-    EXPECT_EQ(WidgetClient::Instance().OnNotice(NoticeType::WIDGET_NOTICE, eventData), ResultCode::SUCCESS);
+    EXPECT_EQ(WidgetClient::Instance().OnNotice(NoticeType::WIDGET_NOTICE, eventData), ResultCode::INVALID_PARAMETERS);
 }
 
-HWTEST_F(WidgetClientTest, WidgetClientTestOnNotice_003, TestSize.Level0)
+HWTEST_F(WidgetClientTest, WidgetClientTestOnNotice_004, TestSize.Level0)
 {
     WidgetNotice widgetNotice;
     widgetNotice.widgetContextId = 1;
@@ -127,10 +137,10 @@ HWTEST_F(WidgetClientTest, WidgetClientTestOnNotice_003, TestSize.Level0)
     widgetNotice.version = "1";
     nlohmann::json root = widgetNotice;
     std::string eventData = root.dump();
-    EXPECT_EQ(WidgetClient::Instance().OnNotice(NoticeType::WIDGET_NOTICE, eventData), ResultCode::SUCCESS);
+    EXPECT_EQ(WidgetClient::Instance().OnNotice(NoticeType::WIDGET_NOTICE, eventData), ResultCode::INVALID_PARAMETERS);
 }
 
-HWTEST_F(WidgetClientTest, WidgetClientTestOnNotice_004, TestSize.Level0)
+HWTEST_F(WidgetClientTest, WidgetClientTestOnNotice_005, TestSize.Level0)
 {
     WidgetNotice widgetNotice;
     widgetNotice.widgetContextId = 1;
@@ -142,7 +152,7 @@ HWTEST_F(WidgetClientTest, WidgetClientTestOnNotice_004, TestSize.Level0)
     EXPECT_EQ(WidgetClient::Instance().OnNotice(NoticeType::WIDGET_NOTICE, eventData), ResultCode::SUCCESS);
 }
 
-HWTEST_F(WidgetClientTest, WidgetClientTestOnNotice_005, TestSize.Level0)
+HWTEST_F(WidgetClientTest, WidgetClientTestOnNotice_006, TestSize.Level0)
 {
     WidgetNotice widgetNotice;
     widgetNotice.widgetContextId = 1;
@@ -154,10 +164,10 @@ HWTEST_F(WidgetClientTest, WidgetClientTestOnNotice_005, TestSize.Level0)
     widgetNotice.version = "1";
     nlohmann::json root = widgetNotice;
     std::string eventData = root.dump();
-    EXPECT_EQ(WidgetClient::Instance().OnNotice(NoticeType::WIDGET_NOTICE, eventData), ResultCode::SUCCESS);
+    EXPECT_EQ(WidgetClient::Instance().OnNotice(NoticeType::WIDGET_NOTICE, eventData), ResultCode::INVALID_PARAMETERS);
 }
 
-HWTEST_F(WidgetClientTest, WidgetClientTestOnNotice_006, TestSize.Level0)
+HWTEST_F(WidgetClientTest, WidgetClientTestOnNotice_007, TestSize.Level0)
 {
     WidgetNotice widgetNotice;
     widgetNotice.widgetContextId = 1;
@@ -172,12 +182,131 @@ HWTEST_F(WidgetClientTest, WidgetClientTestOnNotice_006, TestSize.Level0)
     EXPECT_EQ(WidgetClient::Instance().OnNotice(NoticeType::WIDGET_NOTICE, eventData), ResultCode::INVALID_PARAMETERS);
 }
 
+HWTEST_F(WidgetClientTest, WidgetClientTestOnNotice_008, TestSize.Level0)
+{
+    std::string eventData = "invalid_json_string";
+    EXPECT_EQ(WidgetClient::Instance().OnNotice(NoticeType::WIDGET_NOTICE, eventData), ResultCode::INVALID_PARAMETERS);
+}
+
+HWTEST_F(WidgetClientTest, WidgetClientTestOnNotice_009, TestSize.Level0)
+{
+    WidgetNotice widgetNotice;
+    widgetNotice.widgetContextId = 1;
+    widgetNotice.event = "EVENT_AUTH_USER_NAVIGATION";
+    widgetNotice.typeList.push_back("pin");
+    widgetNotice.typeList.push_back("face");
+    widgetNotice.typeList.push_back("fingerprint");
+    widgetNotice.typeList.push_back("all");
+    widgetNotice.version = "1";
+    std::shared_ptr<WidgetScheduleNode> schedule = nullptr;
+    WidgetClient::Instance().SetWidgetSchedule(schedule);
+    nlohmann::json root = widgetNotice;
+    std::string eventData = root.dump();
+    EXPECT_EQ(WidgetClient::Instance().OnNotice(NoticeType::WIDGET_NOTICE, eventData), ResultCode::INVALID_PARAMETERS);
+}
+
+HWTEST_F(WidgetClientTest, WidgetClientTestOnNotice_0010, TestSize.Level0)
+{
+    WidgetNotice widgetNotice;
+    widgetNotice.widgetContextId = 1;
+    widgetNotice.event = "EVENT_AUTH_USER_NAVIGATION";
+    widgetNotice.typeList.push_back("pin");
+    widgetNotice.typeList.push_back("face");
+    widgetNotice.typeList.push_back("fingerprint");
+    widgetNotice.typeList.push_back("all");
+    widgetNotice.version = "1";
+    nlohmann::json root = widgetNotice;
+    std::string eventData = root.dump();
+    EXPECT_EQ(WidgetClient::Instance().OnNotice(NoticeType::WIDGET_NOTICE, eventData), ResultCode::INVALID_PARAMETERS);
+}
+
+HWTEST_F(WidgetClientTest, WidgetClientTestOnNotice_0011, TestSize.Level0)
+{
+    WidgetNotice widgetNotice;
+    widgetNotice.widgetContextId = 1;
+    widgetNotice.event = "NOTICE_EVENT_CANCEL_AUTH";
+    widgetNotice.typeList.push_back("pin");
+    widgetNotice.typeList.push_back("face");
+    widgetNotice.typeList.push_back("fingerprint");
+    widgetNotice.typeList.push_back("all");
+    widgetNotice.version = "1";
+    std::vector<AuthType> authTypeList;
+    authTypeList.push_back(FACE);
+    authTypeList.push_back(ALL);
+    authTypeList.push_back(PIN);
+    authTypeList.push_back(FINGERPRINT);
+    WidgetClient::Instance().SetAuthTypeList(authTypeList);
+    std::shared_ptr<WidgetScheduleNode> schedule = nullptr;
+    WidgetClient::Instance().SetWidgetSchedule(schedule);
+    nlohmann::json root = widgetNotice;
+    std::string eventData = root.dump();
+    EXPECT_EQ(WidgetClient::Instance().OnNotice(NoticeType::WIDGET_NOTICE, eventData), ResultCode::INVALID_PARAMETERS);
+}
+
+HWTEST_F(WidgetClientTest, WidgetClientTestOnNotice_0012, TestSize.Level0)
+{
+    WidgetNotice widgetNotice;
+    widgetNotice.widgetContextId = 1;
+    widgetNotice.event = "EVENT_AUTH_USER_NAVIGATION";
+    widgetNotice.typeList.push_back("pin");
+    widgetNotice.typeList.push_back("face");
+    widgetNotice.typeList.push_back("fingerprint");
+    widgetNotice.typeList.push_back("all");
+    widgetNotice.version = "1";
+    std::vector<AuthType> authTypeList;
+    authTypeList.push_back(FACE);
+    authTypeList.push_back(ALL);
+    authTypeList.push_back(PIN);
+    authTypeList.push_back(FINGERPRINT);
+    WidgetClient::Instance().SetAuthTypeList(authTypeList);
+    std::shared_ptr<WidgetScheduleNode> schedule = nullptr;
+    WidgetClient::Instance().SetWidgetSchedule(schedule);
+    nlohmann::json root = widgetNotice;
+    std::string eventData = root.dump();
+    EXPECT_EQ(WidgetClient::Instance().OnNotice(NoticeType::WIDGET_NOTICE, eventData), ResultCode::SUCCESS);
+}
+
+HWTEST_F(WidgetClientTest, WidgetClientTestOnNotice_0013, TestSize.Level0)
+{
+    WidgetNotice widgetNotice;
+    widgetNotice.widgetContextId = 1;
+    widgetNotice.event = "NOTICE_EVENT_CANCEL_AUTH";
+    widgetNotice.typeList.push_back("pin");
+    widgetNotice.typeList.push_back("face");
+    widgetNotice.typeList.push_back("fingerprint");
+    widgetNotice.version = "1";
+    std::vector<AuthType> authTypeList;
+    authTypeList.push_back(FACE);
+    authTypeList.push_back(ALL);
+    authTypeList.push_back(PIN);
+    authTypeList.push_back(FINGERPRINT);
+    WidgetClient::Instance().SetAuthTypeList(authTypeList);
+    std::shared_ptr<WidgetScheduleNode> schedule = nullptr;
+    WidgetClient::Instance().SetWidgetSchedule(schedule);
+    nlohmann::json root = widgetNotice;
+    std::string eventData = root.dump();
+    EXPECT_EQ(WidgetClient::Instance().OnNotice(NoticeType::WIDGET_NOTICE, eventData), ResultCode::INVALID_PARAMETERS);
+}
+
+HWTEST_F(WidgetClientTest, WidgetClientTestOnNotice_0014, TestSize.Level0)
+{
+    WidgetNotice widgetNotice;
+    widgetNotice.widgetContextId = 0;
+    widgetNotice.event = "EVENT_AUTH_USER_CANCEL";
+    widgetNotice.typeList.push_back("all");
+    widgetNotice.version = "1";
+    nlohmann::json root = widgetNotice;
+    std::string eventData = root.dump();
+    EXPECT_EQ(WidgetClient::Instance().OnNotice(NoticeType::WIDGET_NOTICE, eventData), ResultCode::INVALID_PARAMETERS);
+}
+
 HWTEST_F(WidgetClientTest, WidgetClientTestReportWidgetResult_001, TestSize.Level0)
 {
     int32_t result = 1;
     int32_t lockoutDuration = 1;
     int32_t remainAttempts = 1;
     WidgetClient::Instance().ReportWidgetResult(result, AuthType::FINGERPRINT, lockoutDuration, remainAttempts);
+    EXPECT_EQ(WidgetClient::Instance().GetAuthTokenId(), 1);
 }
 
 HWTEST_F(WidgetClientTest, WidgetClientTestReportWidgetResult_002, TestSize.Level0)
@@ -187,6 +316,7 @@ HWTEST_F(WidgetClientTest, WidgetClientTestReportWidgetResult_002, TestSize.Leve
     int32_t remainAttempts = 1;
     WidgetClient::Instance().SetSensorInfo("Sensor");
     WidgetClient::Instance().ReportWidgetResult(result, AuthType::FINGERPRINT, lockoutDuration, remainAttempts);
+    EXPECT_EQ(WidgetClient::Instance().GetAuthTokenId(), 1);
 }
 
 HWTEST_F(WidgetClientTest, WidgetClientTestReportWidgetResult_003, TestSize.Level0)
@@ -197,6 +327,7 @@ HWTEST_F(WidgetClientTest, WidgetClientTestReportWidgetResult_003, TestSize.Leve
     WidgetClient::Instance().Reset();
     WidgetClient::Instance().SetSensorInfo("Sensor");
     WidgetClient::Instance().ReportWidgetResult(result, AuthType::FINGERPRINT, lockoutDuration, remainAttempts);
+    EXPECT_EQ(WidgetClient::Instance().GetAuthTokenId(), 0);
 }
 
 HWTEST_F(WidgetClientTest, WidgetClientTestReportWidgetResult_004, TestSize.Level0)
@@ -212,6 +343,57 @@ HWTEST_F(WidgetClientTest, WidgetClientTestReportWidgetResult_004, TestSize.Leve
     EXPECT_EQ(testCallback, nullptr);
     WidgetClient::Instance().SetWidgetCallback(testCallback);
     WidgetClient::Instance().ReportWidgetResult(result, AuthType::PIN, lockoutDuration, remainAttempts);
+    EXPECT_EQ(WidgetClient::Instance().GetAuthTokenId(), 0);
+}
+
+HWTEST_F(WidgetClientTest, WidgetClientTestReportWidgetResult_005, TestSize.Level0)
+{
+    int32_t result = 1;
+    int32_t lockoutDuration = 1;
+    int32_t remainAttempts = 1;
+    WidgetClient::Instance().Reset();
+    MessageParcel data;
+    sptr<IRemoteObject> obj = data.ReadRemoteObject();
+    sptr<WidgetCallbackInterface> testCallback = iface_cast<WidgetCallbackProxy>(obj);
+
+    EXPECT_EQ(testCallback, nullptr);
+    WidgetClient::Instance().SetWidgetCallback(testCallback);
+    WidgetClient::Instance().SetPinSubType(PinSubType::PIN_SIX);
+    WidgetClient::Instance().ReportWidgetResult(result, AuthType::PIN, lockoutDuration, remainAttempts);
+    EXPECT_EQ(WidgetClient::Instance().GetAuthTokenId(), 0);
+}
+
+HWTEST_F(WidgetClientTest, WidgetClientTestReportWidgetResult_006, TestSize.Level0)
+{
+    int32_t result = 1;
+    int32_t lockoutDuration = 1;
+    int32_t remainAttempts = 1;
+    WidgetClient::Instance().Reset();
+    MessageParcel data;
+    sptr<IRemoteObject> obj = data.ReadRemoteObject();
+    sptr<WidgetCallbackInterface> testCallback = iface_cast<WidgetCallbackProxy>(obj);
+
+    EXPECT_EQ(testCallback, nullptr);
+    WidgetClient::Instance().SetWidgetCallback(testCallback);
+    std::vector<AuthType> authTypeList;
+    authTypeList.push_back(FACE);
+    authTypeList.push_back(ALL);
+    authTypeList.push_back(PIN);
+    authTypeList.push_back(FINGERPRINT);
+    WidgetClient::Instance().SetPinSubType(PinSubType::PIN_SIX);
+    WidgetClient::Instance().SetAuthTypeList(authTypeList);
+    WidgetClient::Instance().ReportWidgetResult(result, AuthType::PIN, lockoutDuration, remainAttempts);
+    EXPECT_EQ(WidgetClient::Instance().GetAuthTokenId(), 0);
+}
+
+HWTEST_F(WidgetClientTest, WidgetClientTestForceStopAuth, TestSize.Level0)
+{
+    uint64_t contextId = 6;
+    WidgetClient::Instance().SetWidgetContextId(contextId);
+    auto schedule = std::make_shared<WidgetScheduleNodeImpl>();
+    WidgetClient::Instance().SetWidgetSchedule(schedule);
+    WidgetClient::Instance().ForceStopAuth();
+    EXPECT_EQ(WidgetClient::Instance().GetAuthTokenId(), 0);
 }
 
 HWTEST_F(WidgetClientTest, WidgetClientTestSetPinSubType, TestSize.Level0)
@@ -221,7 +403,8 @@ HWTEST_F(WidgetClientTest, WidgetClientTestSetPinSubType, TestSize.Level0)
     WidgetClient::Instance().SetPinSubType(PinSubType::PIN_MIXED);
     WidgetClient::Instance().SetPinSubType(PinSubType::PIN_MAX);
     WidgetClient::Instance().SetPinSubType((PinSubType)123);
+    EXPECT_EQ(WidgetClient::Instance().GetAuthTokenId(), 0);
 }
-}
-}
-}
+} // namespace UserAuth
+} // namespace UserIam
+} // namespace OHOS
