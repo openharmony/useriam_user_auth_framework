dcc171410727b89eb5fafb2f73adecc51fb87583
liyongfei <liyongfei13@h-partners.com>
2024-06-27 06:13:43 +0000

    fix warning
    Signed-off-by: liyongfei <liyongfei13@h-partners.com>
    Change-Id: Iafc02acbeb25b188fbacf40cd31cf7ae90d0f554

--- services/core/src/schedule_node_impl.h
+++ services/core/src/schedule_node_impl.h
@@ -55,7 +55,7 @@ public:
         bool endAfterFirstFail;
         std::vector<uint8_t> collectorMessage;
         std::vector<uint8_t> verifierMessage;
-        int32_t authIntent;
+        int32_t authIntent {0};
     };
     explicit ScheduleNodeImpl(ScheduleInfo &info);
     ~ScheduleNodeImpl() override;
