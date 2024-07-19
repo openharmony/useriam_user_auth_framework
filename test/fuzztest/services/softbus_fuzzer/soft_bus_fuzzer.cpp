55f016d4f227648ff12a40ee0cd866c4788ec19e
liuhu <liuhu41@h-partners.com>
2024-07-19 10:52:55 +0800

    fuzz告警清理
    Signed-off-by: liuhu <liuhu41@h-partners.com>
    Change-Id: Ia8d6290461111bef62c2b308be0fda278c3455ed

--- test/fuzztest/services/softbus_fuzzer/soft_bus_fuzzer.cpp
+++ test/fuzztest/services/softbus_fuzzer/soft_bus_fuzzer.cpp
@@ -166,7 +166,7 @@ void FuzzSoftBusManagerFisrst(Parcel &parcel)
     auto attributes = Common::MakeShared<Attributes>(attr);
     MsgCallback callback = nullptr;
     softBusManager.SendMessage(connectionName, srcEndPoint, destEndPoint, attributes, callback);
-    std::shared_ptr<BaseSocket> clientSocket = softBusManager.FindClientSocket(connectionName);
+    softBusManager.FindClientSocket(connectionName);
     PeerSocketInfo info;
     softBusManager.OnBind(socketId, info);
     ShutdownReason reason = SHUTDOWN_REASON_LOCAL;
