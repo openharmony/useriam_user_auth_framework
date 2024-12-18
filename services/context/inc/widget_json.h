ed9a883123fcf7699f311750278b4ff9a3402856
李蜜 <limi24@huawei.com>
2024-12-18 21:09:47 +0800

    跨用户身份认证需求
    Signed-off-by: 李蜜 <limi24@huawei.com>

--- services/context/inc/widget_json.h
+++ services/context/inc/widget_json.h
@@ -86,6 +86,7 @@ struct WidgetCommand {
     int32_t isReload {0};
     std::string rotateAuthType {""};
     std::string callingAppID {""};
+    int32_t userId {-1};
 };
 
 void to_json(nlohmann::json &jsonCommand, const WidgetCommand &command);
