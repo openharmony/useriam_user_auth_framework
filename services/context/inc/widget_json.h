13dc04dd57fb76348ca072ba5fdd970211e0015b
HellowWorld <1441418852@qq.com>
2025-03-08 07:32:29 +0000

    update services/context/inc/widget_json.h.
    Signed-off-by: HellowWorld <1441418852@qq.com>

--- services/context/inc/widget_json.h
+++ services/context/inc/widget_json.h
@@ -86,6 +86,7 @@ struct WidgetCommand {
     int32_t isReload {0};
     std::string rotateAuthType {""};
     std::string callingAppID {""};
+    std::string CallingProcessName {""};
     std::string widgetContextIdStr {""};
 };
 
