99a8516de1692b3ca6bc9cd8e0b4bf65d14ffe8e
adelelimi <limi24@huawei.com>
2025-07-28 15:58:15 +0800

    错误码提示信息修改
    Signed-off-by: adelelimi <limi24@huawei.com>

--- frameworks/js/napi/user_auth/src/user_auth_napi_helper.cpp
+++ frameworks/js/napi/user_auth/src/user_auth_napi_helper.cpp
@@ -35,28 +35,6 @@ namespace UserIam {
 namespace UserAuth {
 namespace {
 static constexpr const int MAX_STRING_LENGTH = 65536;
-const std::map<UserAuthResultCode, std::string> g_resultV92Str = {
-    {UserAuthResultCode::OHOS_INVALID_PARAM, "Parameter error."},
-    {UserAuthResultCode::OHOS_CHECK_PERMISSION_FAILED, "Permission denied."},
-    {UserAuthResultCode::OHOS_CHECK_SYSTEM_APP_FAILED, "Permission denied. Called by non-system application."},
-    {UserAuthResultCode::SUCCESS, "Authentication succeeded."},
-    {UserAuthResultCode::FAIL, "Authentication failed."},
-    {UserAuthResultCode::GENERAL_ERROR, "Unknown errors."},
-    {UserAuthResultCode::CANCELED, "Authentication canceled."},
-    {UserAuthResultCode::TIMEOUT, "Authentication timeout."},
-    {UserAuthResultCode::TYPE_NOT_SUPPORT, "Unsupport authentication type."},
-    {UserAuthResultCode::TRUST_LEVEL_NOT_SUPPORT, "Unsupport authentication trust level."},
-    {UserAuthResultCode::BUSY, "Authentication service is busy."},
-    {UserAuthResultCode::PARAM_VERIFIED_FAILED, "The parameter is out of range."},
-    {UserAuthResultCode::LOCKED, "Authentication is lockout."},
-    {UserAuthResultCode::NOT_ENROLLED, "Authentication template has not been enrolled."},
-    {UserAuthResultCode::CANCELED_FROM_WIDGET, "Authentication is canceled from widget."},
-    {UserAuthResultCode::PIN_EXPIRED, "Operation failed because of PIN expired."},
-    {UserAuthResultCode::AUTH_TOKEN_CHECK_FAILED, "Operation failed because of authToken integrity check failed."},
-    {UserAuthResultCode::AUTH_TOKEN_EXPIRED, "Operation failed because of authToken has expired."},
-    {UserAuthResultCode::REUSE_AUTH_RESULT_FAILED, "Failed to reuse authentication result."}
-};
-
 struct DeleteRefHolder {
     napi_env env {nullptr};
     napi_ref ref {nullptr};
