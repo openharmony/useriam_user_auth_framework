39c3e8363e002cdf16ff2a939bf4cf64627b256a
liuhahaofe <liuziwei12@huawei.com>
2025-12-09 20:08:10 +0800

    update: 更新文件 event_listener_callback_service.cpp
    Signed-off-by: liuhahaofe <liuziwei12@huawei.com>

--- frameworks/native/client/src/event_listener_callback_service.cpp
+++ frameworks/native/client/src/event_listener_callback_service.cpp
@@ -28,7 +28,7 @@ int32_t EventListenerCallbackService::OnNotifyAuthSuccessEvent(int32_t userId, i
 {
     auto eventListenerSet = EventListenerCallbackManager<AuthSuccessEventListener>::GetInstance().GetEventListenerSet(
         static_cast<AuthType>(authType));
-    IAM_LOGI("OnNotifyAuthSuccessEvent, userId:%{public}d, authType:%{public}d, callerName:%{public}s"
+    IAM_LOGI("OnNotifyAuthSuccessEvent, userId:%{public}d, authType:%{public}d, callerName:%{public}s,"
         "listenerSize:%{public}zu", userId, authType, callerName.c_str(), eventListenerSet.size());
     for (const auto &listener : eventListenerSet) {
         if (listener == nullptr) {
