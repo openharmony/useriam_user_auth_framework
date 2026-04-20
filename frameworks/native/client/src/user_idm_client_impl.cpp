fbc0d11c658e5f5b6406e02d6d63c10a2253c164
liuhahaofe <liuziwei12@huawei.com>
2026-04-20 19:01:13 +0800

    update: 更新文件 user_idm_client_impl.cpp
    Signed-off-by: liuhahaofe <liuziwei12@huawei.com>

--- frameworks/native/client/src/user_idm_client_impl.cpp
+++ frameworks/native/client/src/user_idm_client_impl.cpp
@@ -378,7 +378,8 @@ int32_t UserIdmClientImpl::RegistCredChangeEventListener(const std::vector<AuthT
 {
     IAM_LOGI("start");
     int32_t ret = IpcClientUtils::RunOnResidentSync([authTypes, listener]() {
-        return EventListenerCallbackManager<CredChangeEventListener>::GetInstance().RegisterListener(authTypes, listener);
+        return EventListenerCallbackManager<CredChangeEventListener>::GetInstance().RegisterListener(
+            authTypes, listener);
     }, MAX_SYNC_WAIT_TIME_SEC);
     return ret;
 }
