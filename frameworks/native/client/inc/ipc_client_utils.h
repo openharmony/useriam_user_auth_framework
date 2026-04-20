bad61a06bab5afe01b8c929ac6c2df122c570958
liuhahaofe <liuziwei12@huawei.com>
2026-04-20 19:17:43 +0800

    update: 更新文件 ipc_client_utils.h
    Signed-off-by: liuhahaofe <liuziwei12@huawei.com>

--- frameworks/native/client/inc/ipc_client_utils.h
+++ frameworks/native/client/inc/ipc_client_utils.h
@@ -16,15 +16,15 @@
 #ifndef IPC_CLIENT_UTILS_H
 #define IPC_CLIENT_UTILS_H
 
-#include "iremote_object.h"
 #include "iam_common_defines.h"
+#include "iremote_object.h"
 
 namespace OHOS {
 namespace UserIam {
 namespace UserAuth {
-class IpcClientUtils {
-constexpr uint32_t MAX_SYNC_WAIT_TIME_SEC = 4; // 4 second
+constexpr uint32_t MAX_SYNC_WAIT_TIME_SEC = 4; // 4 seconds
 
+class IpcClientUtils {
 public:
     static sptr<IRemoteObject> GetRemoteObject(int32_t saId);
     static int32_t RunOnResidentSync(std::function<int32_t> func, uint32_t timeoutSec);
