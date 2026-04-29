38c185ae69b5630af79d423d4107c4a50b00d94d
liuhahaofe <liuziwei12@huawei.com>
2026-04-29 10:13:25 +0800

    update: 更新文件 user_idm_client_impl.cpp
    Signed-off-by: liuhahaofe <liuziwei12@huawei.com>
    
    revert merge request: registCredChange增加超时返回机制
    
    1c49cd3510caaebd2556df678ba1b8808219f232
    
    This revert merge request !1919
    
    revert merge request: 回退registerCredChangeEventListener
    
    2cb1b84c91143c8e0ce988c1629efa35b8b58e0e
    
    This revert merge request !1923

--- test/unittest/inner_api/mocks/mock_ipc_client_utils.h
+++ test/unittest/inner_api/mocks/mock_ipc_client_utils.h
@@ -26,6 +26,7 @@ public:
     static sptr<IRemoteObject> GetRemoteObject(int32_t saId);
     static void SetObj(const sptr<IRemoteObject> &obj);
     static void ResetObj();
+    static int32_t RunOnResidentSync(std::function<int32_t()> func, uint32_t timeoutSec);
 
 private:
     static sptr<IRemoteObject> obj_;
